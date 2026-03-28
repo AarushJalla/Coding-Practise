fe08b6ccff222ebefdc5c225a0d534e69f033c80
AarushJalla <aarushjallalearnings@gmail.com>
2026-03-28 19:00:57 +0000

    Add files via upload

---
+++ Activity 3_input_20260326.py
@@ -0,0 +1,12 @@
+a=input("Enter Your Language:")
+
+b=input("Enter Your Name:")
+
+c=input("Enter Your PIN:")
+
+d=input("Enter The Amount(GBP):")
+
+e="The Transition Was " 
+f="Successful"
+print(e+f)
+
